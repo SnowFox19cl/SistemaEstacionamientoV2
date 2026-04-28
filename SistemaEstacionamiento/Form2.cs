79971e03d9e49a3244dee4acfd589316282ddb13
SnowFox19cl <coolcat14xd@gmail.com>
2026-04-28 15:28:29 -0400

    creacion login y creacion segundo formulario

--- SistemaEstacionamiento/Form2.cs
+++ SistemaEstacionamiento/Form2.cs
@@ -10,19 +10,11 @@ using System.Windows.Forms;
 
 namespace SistemaEstacionamiento
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("bienvenido al sistema");
-            Form2 form = new Form2();
-            form.Show();
-            this.Hide();
         }
     }
 }
